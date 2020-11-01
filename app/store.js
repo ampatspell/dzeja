4ba2bab3a3adad37170b465b1b290e452144bf99
Arnis Vuskans <ampatspell@gmail.com>
2020-11-01 19:41:59 +0200

    configs

--- app/store.js
+++ app/store.js
@@ -2,12 +2,14 @@ import Store from 'zuglet/store';
 
 const options = {
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: "AIzaSyANGrOhzrovbsAPVQ8Wgz4PBSVT6fNm0q0",
+    authDomain: "dzeja-ir.firebaseapp.com",
+    databaseURL: "https://dzeja-ir.firebaseio.com",
+    projectId: "dzeja-ir",
+    storageBucket: "dzeja-ir.appspot.com",
+    messagingSenderId: "347538497489",
+    appId: "1:347538497489:web:76df8760eeac851a098af0",
+    measurementId: "G-D295S9KXFJ"
   },
   firestore: {
     persistenceEnabled: true
@@ -15,15 +17,9 @@ const options = {
   auth: {
     user: 'user'
   },
-  // functions: {
-  //   region: null
-  // },
-  // emulators: {
-  //   host: 'localhost',
-  //   auth: 9099,
-  //   firestore: 8080,
-  //   functions: 5001
-  // }
+  functions: {
+    region: 'europe-west2'
+  }
 };
 
 if(!options.firebase.projectId) {
