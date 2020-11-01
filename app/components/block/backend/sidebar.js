ee7645cb0a8bd32a0e10317d52cc319d00ff7bf1
Arnis Vuskans <ampatspell@gmail.com>
2020-11-01 22:08:52 +0200

    icons

--- app/components/block/backend/sidebar.js
+++ app/components/block/backend/sidebar.js
@@ -1,11 +1,20 @@
 import Component from '@glimmer/component';
 import { tracked } from "@glimmer/tracking"
-import { action } from "@ember/object"
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 
 export default class BlockSidebarComponent extends Component {
 
   @tracked
   stats
+
+  @service
+  router
+
+  @action
+  route(route) {
+    this.router.transitionTo(route);
+  }
 
   @action
   toggleStats() {
